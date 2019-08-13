0f54d6d6a40c39b13b93fa3c0f7c17a2476058e9
Heorhii Siburov <g.siburov@gmail.com>
2019-08-13 13:41:07 -0700

    ThingsButton fixed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   const {num} = props;
-  console.log("NumberButton ", props)
+  // console.log("NumberButton ", props)
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const {spec} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {<button>{spec}</button>}
     </>
   );
 };
+
+export default SpecialButton
